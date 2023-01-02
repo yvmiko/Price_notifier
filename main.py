cb7b8f647ea66bd8ac1fd9d24ee09d56782a91cb
YMKO-lab <120058235+YMKO-lab@users.noreply.github.com>
2023-01-02 09:22:18 +0000

    0.0.1.1

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+
+
+
+# 'requests' library for making HTTP requests 
 import requests
+# 'BeautifulSoup' is a Python library for parsing HTML and XML documents
+# it creates parse trees that are helpful to extract data easily
 from bs4 import BeautifulSoup
+
+# 'smtplib' is a Python library for sending email using the Simple Mail
+# Transfer Protocol.
 import smtplib
+
+# 'time' is a built-in Python library for working with time-related functions
+# It provides functions for getting the current time, converting between different
+# time representations, and measuring elapsed time.
 import time
 
 re = requests.get('')
